294d3863e28becde57515b12f3fc2fc232c74446
gbentham06 <gbentham06@gmail.com>
2025-06-07 04:24:04 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from Basic import basic_calc
 
 if __name__ == '__main__':
+    # demo
     expression = input("Enter expression: ")
     expression = basic_calc(expression)
     print(expression)
-
-#hia
